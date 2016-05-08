9c558a1a58b78aff80009089e839ba841126c051
pjs9322 <gkdlqme@naver.com>
2016-05-08 15:59:54 +0900

    login 프레임에 textfield 추가 글자 크기 조정 및 위치 설정
    double buffring 구현

--- Battle_Rain 0.01/src/view/display.java
+++ Battle_Rain 0.01/src/view/display.java
@@ -8,9 +8,11 @@ import display_Set.display_Set;
 
 public class display extends JFrame implements Runnable {
 	private static final long serialVersionUID = 1L;
-	private STATE state = STATE.Login;
 	public display_Set Current_Display;
+	private int state_code = 0;
 	private Thread thread;
+
+	private STATE state = STATE.Login;
 	
 	public void setState(STATE state) {
 		this.state = state;
@@ -19,7 +21,7 @@ public class display extends JFrame implements Runnable {
 	public display() {										// 생성자 작성
 		thread = new Thread(this);							// 스레드 생성
 		thread.start();										// 스레드 실행
-		this.setTitle(constant.F_Title);						// 프레임 이름 지정
+		this.setTitle(constant.F_Title);					// 프레임 이름 지정
 		this.setSize(constant.F_Width, constant.F_Height); 	// 프레임 사이즈 지정
 		this.setVisible(true);								// 프레임 가시화 옵션 설정
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 창 종료시 프로그램 종료
@@ -29,13 +31,16 @@ public class display extends JFrame implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			while (true) {	
+			while (true) {
 				// 현재 state에 따라 보여주는 display가 달라진다.
-		        Class<?> Next_Display = Class.forName(constant.V_Route + state.toString());
+		        Class<?> Next_Display = Class.forName(constant.V_Route + state.name());
 		        // 현재 state 값에 따라 Next_Display 클래스를 생성
-		        Current_Display = (display_Set) Next_Display.newInstance();
-		        // Next_Display 클래스를 기존 클래스 덮어쓰고 애드온
-				this.add(Current_Display);
+		        if (state_code != state.ordinal()) {
+			        Current_Display = (display_Set) Next_Display.newInstance();
+			        state_code = state.ordinal();
+					this.add(Current_Display);
+			        // state_code의 값이 변경될 경우 (Current_Display와 Next_Display가 달라지는 경우)에 덮어쓴다.
+		        }
 				Thread.sleep(50);
 				// 50 millisecond (0.05sec) 주기 갱신
 				repaint();
